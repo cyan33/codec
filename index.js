2087ea4fdfbecdc78cef05f9ba53aa58e96a8836
thomasyimgit <thomasyim94@gmail.com>
2017-09-24 20:38:42 -0400

    v0.1.0

---
+++ index.js
@@ -0,0 +1,42 @@
+/*
+  codec is a small code-decode utility
+*/
+
+(function(root) {
+  const alphanumeic = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+  // encode
+  let srcToTarget = {};
+  // decode
+  let targetToSrc = {};
+
+  function encode(src) {
+    while (srcToTarget[src]) {
+      let newKey = Array(7).fill(1).reduce((accu, curr) => {
+        return accu + alphanumeic[Math.floor(Math.random() * alphanumeic.length)];
+      }, '')
+
+      if (!targetToSrc[newKey]) {
+        srcToTarget[src] = newKey
+        targetToSrc[newKey] = src
+      }
+    }
+  }
+
+  function decode(key) {
+    return targetToSrc[key];
+  }
+
+  let codec = {
+    encode,
+    decode
+  };
+
+  if (typeof exports === 'object') {
+    module.exports = codec;
+  } else if (typeof define === 'function' && define.amd) {
+    define(function() { return root.codec = codec; })
+  } else {
+    root.codec = codec;
+  }
+})(typeof window === 'object' ? window : global)
